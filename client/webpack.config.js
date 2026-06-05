fa220b6f53ad76a7272968bb191d74d30271096c
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-05 20:59:59 +0500

    fix: add HtmlWebpackPlugin for Vercel deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path'); // Node module: helps with working with file paths.
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -15,4 +16,9 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
 };
