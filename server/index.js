5086d116a0cadf32d07bdd8e16b2c35b0083060c
Mustafa Ali <mu.ali5714@gmail.com>
2026-06-06 22:36:59 +0500

    fix: allow bare repo metadata endpoint in SSRF allowlist

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ app.get('/', (req, res) => {
 
 // GitHub Proxy Route
 // Only allow specific safe endpoints to prevent SSRF
-const ALLOWED_GITHUB_PATHS = /^repos\/[^\/]+\/[^\/]+\/(commits|languages|contributors|tags|stats\/commit_activity)(\/.*)?$/;
+const ALLOWED_GITHUB_PATHS = /^repos\/[^\/]+\/[^\/]+(\/(commits|languages|contributors|tags|stats\/commit_activity)(\/.*)?)?$/;
 
 app.use('/api/github', limiter, async (req, res) => {
   const githubToken = process.env.GITHUB_TOKEN;
